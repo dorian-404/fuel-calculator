f1386f27f992b229bf998aa73502394dc409f7f5
dorian-404 <mishaelwontcheu@gmail.com>
2024-02-12 07:07:01 -0600

    Initial Commit

---
+++ Essence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Essence
+//
+//  Created by user248952 on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Essence/EssenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssenceApp.swift
+//  Essence
+//
+//  Created by user248952 on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EssenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
